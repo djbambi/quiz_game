93875aa751e9c1c64c2b73d4f0ba932478cfe85d
djbambi <djbambrough@mac.com>
2024-08-26 13:20:30 +0100

    logic changed in next_question method and method check_answer added

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,11 +2,19 @@ class QuizBrain:
     def __init__(self, q_list) -> None:
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
+        correct_answer = current_question.answer
         self.question_number += 1
-        input(f'Q.{self.question_number}: {current_question.text} (True/False): ')
+        user_answer = input(
+            f'Q.{self.question_number}: {current_question.text} (True/False): ')
+        return user_answer, correct_answer
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer == correct_answer:
+            self.score += 1
